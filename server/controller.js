12bb3d55cea5bd49ce80549f92c355f0494fd349
Tina Xing <tinaxing14@gmail.com>
2020-05-01 19:24:28 -0400

    staged files

---
+++ server/controller.js
@@ -0,0 +1,11 @@
+const model = require('../db/model.js');
+
+module.exports = {
+  getPrice: (req, res) => {
+    model.fetchPrice(data, )
+  },
+
+  getTripData: (req, res) => {
+
+  },
+};
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const { getPrice, getTripData } = require('./controller.js');
 
 const app = express();
-
-const PORT = 3000;
+const router = express.router();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`\x1b[32m Server listening on PORT\x1b[36m ${PORT}`));
 
@@ -16,3 +17,7 @@ app.use(express.json());
 
 // serving static file
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// route for specific tripid, date and number of people
+router.get('/api/trip/:id/calendar', getPrice);
+router.get('/api/trip/price', getTripData);
