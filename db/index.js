a087a41bbf45d09b55254e504f789b04c291c999
Tina Xing <tinaxing14@gmail.com>
2020-05-01 16:12:42 -0400

    add dbseed js file to automatically insert trip details to database use promise library to print data insertion complete message when all data is inserted.

--- db/index.js
+++ db/index.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
 const mysql = require('mysql');
 
+const Promise = require('bluebird');
+
 const config = require('./config.js');
 
 const connection = mysql.createConnection(config);
 
+Promise.promisifyAll(connection);
 
 connection.connect((err) => {
   if (err) {
