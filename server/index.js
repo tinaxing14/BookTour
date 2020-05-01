2dd787f995b896b27e2f403205d3f97fae97e8ad
Tina Xing <tinaxing14@gmail.com>
2020-05-01 18:07:11 -0400

    staged for a new branch to work on api routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -8,10 +9,10 @@ const PORT = 3000;
 
 app.listen(PORT, () => console.log(`\x1b[32m Server listening on PORT\x1b[36m ${PORT}`));
 
-// logger 
-app.use(morgan("tiny"))
+// logger
+app.use(morgan('tiny'));
 
 app.use(express.json());
 
-// serving static file 
+// serving static file
 app.use(express.static(path.join(__dirname, '../client/dist')));
