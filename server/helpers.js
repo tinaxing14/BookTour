4686497a96d9ad5746905597b9a69bb6216f8a71
Tina Xing <tinaxing14@gmail.com>
2020-05-02 13:35:15 -0400

    modify formateDate function

--- server/helpers.js
+++ server/helpers.js
@@ -1,6 +1,7 @@
 module.exports = {
   // get the current date in "20200501" format
-  formatDate: (today) => {
+  formatDate: () => {
+    const today = new Date();
     const year = today.getFullYear().toString();
     const month = (today.getMonth() + 1).toString().padStart(2, 0);
     const day = today.getDate().toString().padStart(2, 0);
