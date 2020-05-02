6948561634e5051d0f98ccadae92d05152fa1e6d
Tina Xing <tinaxing14@gmail.com>
2020-05-02 11:18:58 -0400

    working on controller functions

--- server/controller.js
+++ server/controller.js
@@ -1,25 +1,31 @@
 const model = require('../db/model.js');
-const { formatDate } = require('./helpers.js')
+const { formatDate } = require('./helpers.js');
 
 module.exports = {
   getPrice: (req, res) => {
+    const { adults, date } = req.query;
+    const tripId = req.params.id;
 
-    model.fetchTripData() => {
-      
-    }) 
+    model.fetchTripData(date, adults, tripId, (err, results) => {
+      if (err) {
+        res.status(500).end();
+      } else {
+        res.json(results);
+      }
+    });
   },
 
   getTripData: (req, res) => {
     const tripId = req.params.id;
-    const today = new Date();
+    let today = new Date();
     today = formatDate(today);
-    
-    model.fetchCurrentTrip(date, tripId, (err, results) => {
+
+    model.fetchCurrentTrip(today, tripId, (err, results) => {
       if (err) {
-        res.status(500).end()
+        res.status(500).end();
       } else {
-        res.json(results)
+        res.json(results);
       }
-    })
+    });
   },
 };
