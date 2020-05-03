7bd5ee0628e9a97836b7107d3d608920a2ffa1a9
Tina Xing <tinaxing14@gmail.com>
2020-05-03 00:58:31 -0400

    modify for tests

--- __tests__/formatDateHelper.test.js
+++ __tests__/formatDateHelper.test.js
@@ -10,7 +10,7 @@ describe('Testing helper function formatDate', () => {
   });
   it('should have the correct order of year, month and day', () => {
     expect(formatDate().slice(0, 4)).toBe('2020');
-    expect(formatDate().slice(4, 6)).toBe('05');
-    expect(formatDate().slice(6, 8)).toBe('02');
+    expect(Number(formatDate().slice(4, 6))).toBeLessThan(12);
+    expect(Number(formatDate().slice(6, 8))).toBeLessThan(31);
   });
 });
--- server/helpers.js
+++ server/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
     const today = new Date();
     const year = today.getFullYear().toString();
     const month = (today.getMonth() + 1).toString().padStart(2, 0);
-    const day = today.getDate().toString().padStart(2, 0);
+    const day = (today.getDate() + 1).toString().padStart(2, 0);
     const date = year + month + day;
     return date;
   },
