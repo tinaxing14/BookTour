c646d8fa8b6118e7cde10729807ca60a03f1d8c9
Tina Xing <tinaxing14@gmail.com>
2020-05-04 21:37:10 -0400

    modify helper

--- client/src/http.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-module.exports = {
-  fetchTripData: (id) => axios.get(`/api/trip/${id}/price`)
-    .then((response) => response.data),
-
-  fetchPrice: (id, date, adults) => axios.get(`/api/trip/${id}/calendar/?startdate=${date}&enddate=${date}&adults=${adults}`)
-    .then((response) => response.data),
-};
