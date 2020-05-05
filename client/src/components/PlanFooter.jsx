239c013d5ee50e5344438d5af67d72fbf8780be1
Tina Xing <tinaxing14@gmail.com>
2020-05-04 21:41:02 -0400

    modify footer getdate function

--- client/src/components/PlanFooter.jsx
+++ client/src/components/PlanFooter.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import styles from '../styles/PlanFooter.css';
+import date from '../helpers/getDate.js';
 
 class PlanFooter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
-
+  
   render() {
     return (
       <div className={styles.planfooter}>
@@ -15,11 +16,12 @@ class PlanFooter extends React.Component {
         <div className={styles.cancelation}>
           <div className={styles.cancelation_btn}>Free Cancelation</div>
           <div className={styles.cancelation_term}>
-            Risk free 100% refund if you cancel by !Aug 18
+            {`Risk free 100% refund if you cancel by 
+            ${date.getCancelationDate(this.props.cancelation)}`}
           </div>
         </div>
         <div className={styles.popularity}>
-          <div className={styles.popularity_number}>Popular: Booked by !3059 travelers</div>
+          <div className={styles.popularity_number}>{`Popular: Booked by ${this.props.totalBooked} travelers`}</div>
           <div>
             <span className={styles.checkicon} />
             <span className={styles.popularity_lowprice}>Low Price Guarantee</span>
