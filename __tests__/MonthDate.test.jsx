67a29a8343253877f9455b07574efc10584e7bf7
Tina Xing <tinaxing14@gmail.com>
2020-05-06 15:28:41 -0400

    state

--- __tests__/MonthDate.test.jsx
+++ __tests__/MonthDate.test.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import MonthDate from '../client/src/components/MonthDate.jsx';
+import MonthDate from "../client/src/components/MonthDate";
 import Months from '../client/src/components/Months.jsx';
 import Days from '../client/src/components/Days.jsx';
 
-const sampleMonthYear = ["May", 2020];
+const sampleMonthYear = ['May', 2020];
 test('should render Months and Days components', () => {
-    const wrapper = shallow(
-      <MonthDate
-        monthYear={sampleMonthYear}
-        handleDayClick={() => {}}
-      />
-    );
-    expect(wrapper.find(Months)).toHaveLength(1);
-    expect(wrapper.find(Days)).toHaveLength(1);
-    expect(wrapper.find('.month')).toHaveLength(1);
-    expect(wrapper.find('.date')).toHaveLength(1);
-  });
-  
+  const wrapper = shallow(
+    <MonthDate
+      monthYear={sampleMonthYear}
+      handleDayClick={() => {}}
+    />
+  );
+  expect(wrapper.find(Months)).toHaveLength(1);
+  expect(wrapper.find(Days)).toHaveLength(1);
+  expect(wrapper.find('.month')).toHaveLength(1);
+  expect(wrapper.find('.date')).toHaveLength(1);
+});
--- client/src/components/MonthDate.jsx
+++ client/src/components/MonthDate.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from '../styles/MonthDate.css';
 import Months from './Months.jsx';
 import Days from './Days.jsx';
-const MonthDate = ({ monthYear, handleDayClick }) => (
-  <div className={styles.container}>
+const MonthDate = ({ monthYear, handleDayClick }) => (<div className={styles.container}>
     <div className={styles.month}>
-        <Months monthYear={monthYear}/>
+        <Months monthYear={monthYear} />
     </div>
     <div className={styles.date}>
-        <Days monthYear={monthYear} handleDayClick={handleDayClick}/>
+        <Days monthYear={monthYear} handleDayClick={handleDayClick} />
     </div>
-  </div>
-);
-
+</div>);
 export default MonthDate;
