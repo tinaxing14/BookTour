6b8bc6c193991f6cdab572e32fc440a86a5b48c3
Tina Xing <tinaxing14@gmail.com>
2020-05-07 22:39:37 -0400

    add tests

--- __tests__/PlanFooter.test.jsx
+++ __tests__/PlanFooter.test.jsx
@@ -1,20 +1,65 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import PlanFooter from '../client/src/components/PlanFooter.jsx';
 import styles from '../client/src/styles/PlanFooter.css';
 
 let wrapper;
 const sampleTotalBooked = 3987;
 const sampleCancelation = 24;
+const sampleSelectDate = ['May', 7, 2020];
+const mockGoNext = jest.fn();
+let sampleAdults = false;
+let sampleCalendar = false;
+let sampleOptions = true;
 
-beforeEach(() => {
-  wrapper = shallow(<PlanFooter totalBooked={sampleTotalBooked} cancelation={sampleCancelation} />)
+describe('Tests for PlanFooter Component', () => {
+
+  beforeEach(() => {
+    wrapper = shallow(<PlanFooter totalBooked={sampleTotalBooked} cancelation={sampleCancelation} showCalendar={sampleCalendar} showAdults={sampleAdults} showOptions={sampleOptions} goNext={mockGoNext} selectDate={sampleSelectDate} />)
+  })
+
+  test('should render correct cancelations date', () => {
+    expect(wrapper.find("[data-test='cancelterm']").text()).toEqual('Risk free 100% refund if you cancel by May 8');
+  });
+  
+  test('should render correct total booked number', () => {
+    expect(wrapper.find("[data-test='popnum']").text()).toBe('Popular: Booked by 3987 travelers')
+  });
+
+  test('should render correct button text when prop changes, render Book Now', () => {
+    expect(wrapper.find("[data-test='updatebtn']").text()).toBe('Book Now');
+  });
+  
+  test('should render correct button text when prop changesm render Next', ()=>{
+    sampleOptions = false;
+    sampleAdults = true;
+    wrapper.setProps({showOptions: sampleOptions, showAdults: sampleAdults});
+    expect(wrapper.find("[data-test='updatebtn']").text()).toBe('Next');
+    sampleAdults = false;
+    sampleCalendar = true;
+    wrapper.setProps({showAdults:sampleAdults, showCalendar:sampleCalendar});
+    expect(wrapper.find("[data-test='updatebtn']").text()).toBe('Next');
+  });
+
+  test('should render correct button text when prop changes, render Check Availability', ()=>{
+    sampleOptions = false;
+    sampleAdults = false;
+    sampleCalendar =false;
+    wrapper.setProps({showOptions: sampleOptions, showAdults: sampleAdults, showCalendar: sampleCalendar});
+    expect(wrapper.find("[data-test='updatebtn']").text()).toBe('Check Availability');
+  });
+
+  test('should invoke go next function when button is clicked', () => {
+    wrapper.find("[data-test='updatebtn']").simulate('click');
+    expect(mockGoNext).toHaveBeenCalled();
+  });
+
 })
 
-// test('should render correct cancelations date', () => {
-//   expect(wrapper.find(`.${styles.cancelation_term}`).text()).toBe('Risk free 100% refund if you cancel by May 5');
-// });
 
-test('should render correct total booked number', () => {
-  expect(wrapper.find(`.${styles.popularity_number}`).text()).toBe('Popular: Booked by 3987 travelers')
-});
+
+
+
+
+
+
--- __tests__/SelectHeader.test.jsx
+++ __tests__/SelectHeader.test.jsx
@@ -2,3 +2,40 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SelectHeader from '../client/src/components/SelectHeader.jsx';
 import styles from '../client/src/styles/SelectHeader.css';
+
+let wrapper;
+const samplePrice = 189;
+const sampleSelectDate = ['Aug', 23, 2020];
+const sampleSelectAdults = 3;
+const mockHandleCalendarClick = jest.fn();
+describe('Tests for SelectHeader Component', () => {
+  beforeEach(() => {
+    wrapper = shallow(<SelectHeader price={samplePrice} selectDate={sampleSelectDate} selectAdults={sampleSelectAdults} handleCalendarClick={mockHandleCalendarClick}/>);
+  });
+
+  test('should render correct price', ()=> {
+    expect(wrapper.find("[data-test='price']").text()).toBe('$189');
+  });
+
+  test('should render three buttons on the page', () => {
+    expect(wrapper.find("[data-test='optionsButton']")).toHaveLength(3);
+  });
+
+  test('should render the correct date', () => {
+    expect(wrapper.find("[data-test='date']").text()).toBe('Aug 23')
+  });
+
+  test('should render correct number of adults', () => {
+    expect(wrapper.find("[data-test='adults']").text()).toBe('3  Adults');
+  });
+
+  test('should render correct text of options',() => {
+    expect(wrapper.find("[data-test='options']").text()).toBe('Options')
+  });
+
+  test('should invoke function handleCalendarClick when the first button is clicked', ()=> {
+    wrapper.find("[data-test='handleClick']").simulate('click');
+    expect(mockHandleCalendarClick).toHaveBeenCalled();
+  })
+
+})
