2f797e05a635c6be9730e6bce55b057cd4d83c47
Tina Xing <tinaxing14@gmail.com>
2020-05-10 16:35:25 -0400

    keep promise functions

--- server/controller.js
+++ server/controller.js
@@ -4,33 +4,9 @@ const model = require('../db/model.js');
 const { formatDate } = require('./helpers.js');
 
 module.exports = {
-  getPrice: (req, res) => {
-    const { adults, startdate } = req.query;
-    const tripId = req.params.id;
-    model.fetchTripData(startdate, tripId, adults, (err, results) => {
-      if (err) {
-        res.status(500).end();
-      } else {
-        res.json(results);
-      }
-    });
-  },
-
-  getTripData: (req, res) => {
-    const tripId = req.params.id;
-    const today = formatDate();
-    model.fetchCurrentTrip(today, tripId, (err, results) => {
-      if (err) {
-        res.status(500).end();
-      } else {
-        res.json(results);
-      }
-    });
-  },
-
   // promise function for testing. resolve err and success for functionality test
 
-  getTripDataAsync: (req, res) => new Promise((resolve, reject) => {
+  getTripData: (req, res) => new Promise((resolve, reject) => {
     const tripId = req.params.id;
     const today = formatDate();
     model.fetchCurrentTrip(today, tripId, (err, results) => {
@@ -42,7 +18,7 @@ module.exports = {
     });
   }),
 
-  getPriceAsync: (req, res) => new Promise((resolve, reject) => {
+  getPrice: (req, res) => new Promise((resolve, reject) => {
     const { adults, startdate } = req.query;
     const tripId = req.params.id;
     model.fetchTripData(startdate, tripId, adults, (err, results) => {
