1a47bfbd940b83de54c32c72f41cbf7e21e0f3ef
Tina Xing <tinaxing14@gmail.com>
2020-05-10 20:33:37 -0400

    refactor to stateful component, add toggleRead function and readmore div

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
-import styles from '../styles/Options.css'
+import styles from '../styles/Options.css';
 
-const Options = ({tripData, selectAdults}) => (
-  <div className={styles.container}>
-    <div className={styles.detail}>
-      <div className={styles.content}>
-        <div className={styles.title}>{tripData.duration}</div>
-        <div className={styles.tripdetail}>{tripData.detail}</div>
+class Options extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      contentWrap: styles.tripdetail,
+      readmore:false,
+    }
+    this.toggleRead = this.toggleRead.bind(this);
+  }
+  toggleRead() {
+    this.setState({
+      contentWrap: styles.tripdetail_change,
+      readmore: !this.state.readmore,
+    });
+
+  }
+
+  render() {
+    const {tripData, selectAdults} = this.props;
+    return (
+      <div className={styles.container}>
+      <div className={styles.detail}>
+        <div className={styles.content}>
+          <div data-test='tripDuration' className={styles.title}>{tripData.duration}</div>
+          <div data-test='tripDetail' className={this.state.contentWrap}>{tripData.detail}</div>
+          {this.state.readmore ? null : <div onClick={this.toggleRead} className={styles.read}>Read More</div>}
+        </div>
+        <div className={styles.price}>
+          <div className={styles.total}>Total</div>
+          <div data-test='tripPrice' className={styles.dollar}>{'$' + tripData.price*selectAdults}</div>
+      <div data-test='tripPriceCal' className={styles.calculate}>{`${selectAdults}Adults x $${tripData.price}`}</div>
+        </div>
+        <div className={styles.check}>
+        <span className={styles.checkmark}>
+        <div className={styles.checkmark_circle}></div>
+        <div className={styles.checkmark_stem}></div>
+        <div className={styles.checkmark_kick}></div>
+        </span>
+        </div>
       </div>
-      <div className={styles.price}>
-        <div className={styles.total}>Total</div>
-        <div className={styles.dollar}>{'$' + tripData.price*selectAdults}</div>
-<div className={styles.calculate}>{`${selectAdults}Adults x $${tripData.price}`}</div>
+      <div data-test='tripTime' className={styles.time}>
+        {tripData.duration.slice(0,7)}
       </div>
-      <div className={styles.check}>
-      <span class={styles.checkmark}>
-      <div class={styles.checkmark_circle}></div>
-      <div class={styles.checkmark_stem}></div>
-      <div class={styles.checkmark_kick}></div>
-      </span>
-      </div>
-    </div>
-    <div className={styles.time}>
-      {tripData.duration.slice(0,7)}
-    </div>
-  </div>
-)
+    </div>      
+    )
+  }
+};
 
 export default Options;
