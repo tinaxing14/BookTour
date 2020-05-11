46de6fb4fe9d3579b1f9b487807a044690586997
Tina Xing <tinaxing14@gmail.com>
2020-05-10 20:32:51 -0400

    add data-test selector

--- client/src/components/SelectHeader.jsx
+++ client/src/components/SelectHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/SelectHeader.css';
 
-const SelectHeader = ({price, selectDate, selectAdults, handleCalendarClick}) => (
+const SelectHeader = ({price, selectDate, selectAdults, handleCalendarClick, showAdults, showOptions, showCalendar, handleAdultsClick, handleOptionsClick}) => (
   <div className={styles.header}>
         <div className={styles.header_price}>
           <div className={styles.date_travelers}>Select Date and Travelers</div>
@@ -15,15 +15,15 @@ const SelectHeader = ({price, selectDate, selectAdults, handleCalendarClick}) =>
           </div>
         </div>
         <div data-test='optionsButton'className={styles.dates_adults_options}>
-          <div data-test='handleClick' className={styles.dates_adults_options_btn} onClick={handleCalendarClick}>
+          <div data-test='handleClick' className={showCalendar ? styles.dates_adults_options_btn_change : styles.dates_adults_options_btn} onClick={handleCalendarClick}>
             <div className={`gg-calendar-dates ${styles.icons}`}/>
             <span data-test='date'>{selectDate[0] + ' ' + selectDate[1]}</span>
           </div>
-          <div data-test='optionsButton' className={styles.dates_adults_options_btn}>
+          <div data-test='optionsButton' className={showAdults ? styles.dates_adults_options_btn_change : styles.dates_adults_options_btn} onClick={handleAdultsClick}>
             <div className={`gg-user-add ${styles.icons}`} />
             <span data-test='adults'>{selectAdults + '  Adults'}</span>
           </div>
-          <div data-test='optionsButton' className={styles.dates_adults_options_btn}>
+          <div data-test='optionsButton' className={showOptions ? styles.dates_adults_options_btn_change : styles.dates_adults_options_btn} onClick={handleOptionsClick}>
             <div className={`gg-awards ${styles.icons}`} />
             <span data-test='options'>Options</span>
           </div>
