60b1ff520e51c0d27c96c03509e8b40965575765
Tina Xing <tinaxing14@gmail.com>
2020-05-10 20:34:49 -0400

    add datatest selector for testing

--- client/src/components/Adults.jsx
+++ client/src/components/Adults.jsx
@@ -9,9 +9,9 @@ const Adults = ({minusOne, plusOne, selectAdults, selectChild}) => (
       <div className={styles.age}>Age 13 - 99</div>
     </div>
       <div className={styles.number}>
-        <div className={styles.minus_plus} ><input type='button' name='selectAdults' onClick={minusOne} className={styles.button} value='-' /></div>
-        <div className={styles.digit}>{selectAdults}</div>
-        <div className={styles.minus_plus }><input type='button' name='selectAdults' onClick={plusOne} className={styles.button} value='+' /></div>
+        <div className={styles.minus_plus}><input data-test='adultsminus' type='button' name='selectAdults' onClick={minusOne} className={styles.button} value='-' /></div>
+        <div data-test='adultDigit' className={styles.digit}>{selectAdults}</div>
+        <div className={styles.minus_plus}><input data-test='adultsplus' type='button' name='selectAdults' onClick={plusOne} className={styles.button} value='+' /></div>
       </div>
   </div>
   <div className={styles.adult_child}>
@@ -20,13 +20,13 @@ const Adults = ({minusOne, plusOne, selectAdults, selectChild}) => (
       <div className={styles.age}>Age 0 - 12</div>
     </div>
       <div className={styles.number}>
-        <div className={styles.minus_plus} ><input type='button' name='selectChild' onClick={minusOne} className={styles.button} value='-' /></div>
-        <div className={styles.digit}>{selectChild}</div>
-        <div className={styles.minus_plus} ><input type='button' name='selectChild' onClick={plusOne} className={styles.button} value='+' /></div>
+        <div className={styles.minus_plus} ><input data-test='childminus' type='button' name='selectChild' onClick={minusOne} className={styles.button} value='-' /></div>
+        <div data-test='childDigit' className={styles.digit}>{selectChild}</div>
+        <div className={styles.minus_plus} ><input data-test='childplus' type='button' name='selectChild' onClick={plusOne} className={styles.button} value='+' /></div>
       </div>
   </div>
 </div>
-)
+);
 
 
 export default Adults;
