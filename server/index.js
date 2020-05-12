5d6d58badf89337b4c0b23500be9fda89bf4e11a
Tina Xing <tinaxing14@gmail.com>
2020-05-12 19:39:10 -0400

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./routes.js');
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`\x1b[32m Server listening on PORT\x1b[36m ${PORT}`));
 
+app.use(cors());
 // logger
 app.use(morgan('tiny'));
 
