0a203b82fe3e19579ed60c952f6f04e2cffff7b1
Tina Xing <tinaxing14@gmail.com>
2020-05-16 21:08:47 -0400

    add compression

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./routes.js');
-const cors = require('cors')
+const cors = require('cors');
+const compression = require('compression');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,11 +17,14 @@ app.use(morgan('tiny'));
 
 // request body parser
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(compression());
 
 
 // serving static file
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
 
 // use router to route the requests to same path
 app.use('/api/trip', router);
